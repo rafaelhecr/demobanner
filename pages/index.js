0658e14d666f3179770c76351f36de3ee9eb7747
rafael-h <rafaelhecr@outlook.com>
2022-10-25 21:48:01 -0500

    cms headless

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,30 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home({news}) {
+export default function Home({items, news}) {
   return (
     <>
       <div className='well-white'>
         <h5>Learn More From <br/> Keith Cunninham</h5>
-        {
+        {/* {
           news.map(n => (
             <>
               <p>
                 <a target="_blank" rel="noreferrer" href="http://keystothevault.com/events/">{n.text}</a>
+              </p>
+            </>
+          ))
+        } */}
+        {
+          items.map((n,i) => (
+            <>
+              <p>
+                <a key={i} 
+                target="_blank" 
+                rel="noreferrer" 
+                href={n.fields.demo3}>
+                  {n.fields.nameOfNews}
+                </a>
               </p>
             </>
           ))
@@ -22,6 +36,12 @@ export default function Home({news}) {
 }
 
 export async function getStaticProps(){
+  
+    const res = await fetch('https://cdn.contentful.com/spaces/dpfyxoc71hxt/environments/master/entries?access_token=_KjWIpr0v1hxJ-QIBjHE_RCJSZE5nTaUiV4BseFMzfk')
+    const response = await res.json();
+    const {items} = response
+
+  
 
   const news = [
     {text: 'Plan or Get Slaughtered'},
@@ -31,7 +51,7 @@ export async function getStaticProps(){
 
   return{
     props:{
-      news
+      items, news
     }
   }
 }
